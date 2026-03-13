7eff32ed723ec59926ad72bf14fbd62056184eea
Maxi Schultz <macuss@gmail.com>
2026-03-13 17:27:40 -0300

    Refactor: DBConnection ahora usa config.properties para mayor seguridad

--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -1,15 +1,29 @@
 package util;
 
+import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/testmanager";
-    private static final String USER = "root";
-    private static final String PASSWORD = "1234"; // 
+    private static Properties props = new Properties();
+
+    static {
+        try {
+     
+            FileInputStream fis = new FileInputStream("src/config.properties");
+            props.load(fis);
+        } catch (Exception e) {
+            System.err.println("Error cargando config.properties: " + e.getMessage());
+        }
+    }
 
     public static Connection getConnection() throws Exception {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(
+            props.getProperty("db.url"),
+            props.getProperty("db.user"),
+            props.getProperty("db.password")
+        );
     }
 }
